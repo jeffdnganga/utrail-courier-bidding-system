0de01937ec1d11cd0e1de1d00c1e1bdbc6e8fdc6
jeffdnganga <jeffdaniels950@gmail.com>
2021-09-15 16:18:55 +0300

    Added a new row to bids page

--- bids.php
+++ bids.php
@@ -26,7 +26,7 @@ include('includes/client_navigation.php');
             </select>            
         </div>
 
-        <div class="row bid">
+        <div class="row bid mb-3">
             <div class="col-sm-12 col-md-8 col1">
                 <div class="bidder-profile">
                     <div class="bidder-image">
@@ -51,7 +51,31 @@ include('includes/client_navigation.php');
                 <button type="button" class="btn book-btn btn-success d-block" data-bs-toggle="modal" data-bs-target="#paymentModal">Proceed and Book <i class="fas fa-thumbs-up ps-2"></i></button>
             </div>
         </div>
+        <div class="row bid mb-3">
+            <div class="col-sm-12 col-md-8 col1">
+                <div class="bidder-profile">
+                    <div class="bidder-image">
+                        <img src="assets/images/user-image.jpg" alt="Bidder's picture">
+                    </div>
+                    <p class="bidder-name ps-2 pt-2">Matthew</p>
+                </div>
 
+                <p class="bidder-message">Hi, I'm bidding providing that you will only need my van and I for assistance, if more items are added to whats originally stated later on, a new price will have to be reviewed.</p>
+
+                <p class="bidder-contact"><span class="smaller-text">Call to discuss this bid:</span> <br> <i class="fas fa-phone-alt pe-1"></i>0712345678</p>
+
+            </div>
+            <div class="col-sm-12 col-md-4 col2">
+                <p class="bid-amount" style="font-size: 14px;">
+                    <span class="full-amount h5" style="font-size: 18px;">Ksh 1,500</span> <span style="font-weight: 500; color: #EE6C4D;">Total Price</span> <br> Book now for only <span style="font-size: 16px; font-weight: 500; color: #EE6C4D;">Ksh 800</span> deposit!
+                </p>
+
+                <p class="bid-summary" style="font-size: 16px; font-weight: 500; color: #EE6C4D; margin-bottom: 0px;">Bid Summary</p>
+                <p class="vehicle-type" style="margin-bottom: 0px; font-size: 14px;"><i class="fas fa-truck-pickup pe-2"></i>Isuzu pickup</p>
+                <p class="proposed-date" style="font-size: 14px;"><i class="fas fa-calendar-alt pe-2"></i>Flexible with dates</p>
+                <button type="button" class="btn book-btn btn-success d-block" data-bs-toggle="modal" data-bs-target="#paymentModal">Proceed and Book <i class="fas fa-thumbs-up ps-2"></i></button>
+            </div>
+        </div>
     </div>
 </section>
 
